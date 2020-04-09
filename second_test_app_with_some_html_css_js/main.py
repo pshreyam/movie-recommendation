b76abf05573f25c9368be723824a479f0cea4dae
pshreyam <sp02434318@student.ku.edu.np>
2020-04-09 14:13:45 +0545

    second flsk app

---
+++ second_test_app_with_some_html_css_js/main.py
@@ -0,0 +1,24 @@
+from flask import (
+    Flask,
+    render_template,
+    Request,
+    redirect,
+    abort
+) 
+Flask,render_template,Request,redirect
+
+app=Flask(__name__) 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/mission")
+def mission():
+    return render_template("mission.html")
+
+
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
