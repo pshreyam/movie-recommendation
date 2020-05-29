495cc74a6e6b3e5d82131f438e83f09af21bd20f
pshreyam <sp02434318@student.ku.edu.np>
2020-05-29 16:33:04 +0545

    some methods added

--- submodules/handleLogin.py
+++ submodules/handleLogin.py
@@ -1,23 +1,20 @@
 import mysql.connector as db
 
 conn = db.connect(
-	host = "localhost",
+    host = "localhost",
     user = "root",
     password="",
     database = "recomovies"
 )
 
 def doLogin(username,password):
-	cursor = conn.cursor()
-	sql = f"SELECT * FROM user WHERE username = '{username.lower()}'"
-	cursor.execute(sql)
-	records = cursor.fetchall()
-	if not records:
-		return False,f"No registered username as {username}!"
-	if not (password == records[0][6]):
-		return False,f"Password doesn't seem to match. Please try again."
-	return True,None
-
-
-
-
+    cursor = conn.cursor()
+    sql = f"SELECT * FROM user WHERE username = '{username.lower()}'"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    if not records:
+        return False, f"No registered username as {username}!"
+    db_pass = records[0][6]
+    if not (password == db_pass):
+        return False, f"Password doesn't seem to match. Please try again."
+    return True, None
