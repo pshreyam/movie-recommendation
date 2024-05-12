60b2205a99cce62d3dd1c14f52b1f1aa21876bfb
Shreyam Pokharel <pshreyam@gmail.com>
2024-05-12 21:08:19 +0545

    Revert "pagination moved here from another repo"
    This reverts commit 3c13b40e29f8c32ef99797933fd891481af8ddb3.

--- pagination/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-config = {
-    "host": os.environ.get('DB_HOST', ''),
-    "user": os.environ.get('DB_USER', ''),
-    "password": os.environ.get('DB_PASSWORD', ''),
-    "database": os.environ.get('DB_NAME', '')
-}
