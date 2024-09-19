a67678aa0216d1bfc330b349a4c1364bad4c56d4
Shreyam Pokharel <pshreyam@gmail.com>
2024-09-19 22:41:33 +0545

    Add sample env variable file

---
+++ env-sample.sh
@@ -0,0 +1,7 @@
+# Define the environment variables to connect to the db
+
+export MYSQL_ROOT_PASSWORD=admin
+export MYSQL_USER=test
+export MYSQL_PASSWORD=test
+export MYSQL_DATABASE=movie-recommendation
+export SECRET_KEY=1234
