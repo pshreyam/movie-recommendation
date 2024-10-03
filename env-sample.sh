3b88a44bd6a81e6f5ad1016f0c51f3e74e3a6b49
Shreyam Pokharel <pshreyam@gmail.com>
2024-10-03 17:20:56 +0545

    Add cloudinary specific environment variables

--- env-sample.sh
+++ env-sample.sh
@@ -5,3 +5,6 @@ export MYSQL_USER=test
 export MYSQL_PASSWORD=test
 export MYSQL_DATABASE=movie-recommendation
 export SECRET_KEY=1234
+export CLOUDINARY_API_SECRET=test_cloudinary_api_secret
+export CLOUDINARY_API_KEY=test_cloudinary_api_key
+export CLOUDINARY_CLOUD_NAME=test_cloudinary_cloud_name
